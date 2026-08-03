0aedc4e9379fec3c27fe74bb4ff28478abd33e87
Haaa-JPG <mrfhamhgeneralknowledge2@gmail.com>
2026-08-04 01:23:29 +0300

    fix: fully open CSP to allow all resources

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
           { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=(), interest-cohort=()" },
           { key: "X-XSS-Protection", value: "1; mode=block" },
           { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
-          { key: "Content-Security-Policy", value: "default-src 'self' https://*.supabase.co; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.youtube.com https://player.vimeo.com https://www.dailymotion.com https://*.wistia.com; style-src 'self' 'unsafe-inline'; img-src 'self' https://images.unsplash.com https://*.supabase.co data: blob: http: https:; font-src 'self'; connect-src 'self' https://*.supabase.co wss://*.supabase.co; frame-src https://www.youtube.com https://player.vimeo.com https://www.dailymotion.com https://*.wistia.com http: https:; frame-ancestors 'self';" },
+          { key: "Content-Security-Policy", value: "default-src * 'self' 'unsafe-inline' 'unsafe-eval' data: blob:; img-src * data: blob: http: https:; connect-src *; frame-src * http: https:; media-src * http: https:; script-src * 'self' 'unsafe-inline' 'unsafe-eval'; style-src * 'self' 'unsafe-inline';" },
         ],
       },
       {
